9395189aa0d506db0ffa64f96bae2f6f47c0858a
李胜营 <lishengying@lishengyingdeMacBook-Air-2.local>
2016-05-18 11:02:34 +0800

    初始化

---
+++ 百思不得姐/百思不得姐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得姐
+//
+//  Created by 李胜营 on 16/5/18.
+//  Copyright (c) 2016年 dasheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
